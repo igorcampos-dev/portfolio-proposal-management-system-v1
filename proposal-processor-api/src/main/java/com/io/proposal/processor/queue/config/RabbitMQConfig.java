ef24531177d30f8bb77830452970e6d234d832c7
igorcampos-dev <igorccampos8@gmail.com>
2024-10-24 09:44:27 -0300

    refactor: melhorias no rabbitmq e criacao dlx para o consumer em casos de erros

--- proposal-processor-api/src/main/java/com/io/proposal/processor/queue/config/RabbitMQConfig.java
+++ proposal-processor-api/src/main/java/com/io/proposal/processor/queue/config/RabbitMQConfig.java
@@ -1,8 +1,10 @@
 package com.io.proposal.processor.queue.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,12 +22,18 @@ public class RabbitMQConfig {
 
     @Bean
     public Queue queue (){
-        return new Queue(queue, true);
+        return QueueBuilder.durable(queue)
+                           .withArgument("x-dead-letter-exchange", "dead_letter_exchange")
+                           .withArgument("x-dead-letter-routing-key", "dead_letter_key")
+                           .build();
+
+
     }
 
     @Bean
     public Jackson2JsonMessageConverter messageConverter(){
-      return new Jackson2JsonMessageConverter(mapper);
+        mapper.registerModule(new JavaTimeModule());
+        return new Jackson2JsonMessageConverter(mapper);
     }
 
 }
--- proposal-processor-api/src/main/java/com/io/proposal/processor/queue/consumer/ProposalsConsumer.java
+++ proposal-processor-api/src/main/java/com/io/proposal/processor/queue/consumer/ProposalsConsumer.java
@@ -1,17 +1,24 @@
 package com.io.proposal.processor.queue.consumer;
 
 import com.io.proposal.processor.domain.dto.ProposalDto;
+import com.rabbitmq.client.Channel;
 import jakarta.validation.Valid;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import java.io.IOException;
 
 @Component
 public class ProposalsConsumer {
 
     @RabbitListener(queues = "${spring.rabbitmq.template.queue}")
-    public void listenEmailQueue(@Valid @Payload ProposalDto dto){
-        System.out.println(String.format("Proposal received: %s", dto));
+    public void listenEmailQueue(@Valid @Payload ProposalDto dto, Channel channel, Message message) throws IOException {
+        try {
+            System.out.println(String.format("Proposal received: %s", dto));
+        } catch (Exception e){
+            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+        }
     }
 
 }
