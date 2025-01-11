34fd4167f2015bded0d9297523627f035ba6eda2
igorcampos-dev <igorccampos8@gmail.com>
2025-01-10 22:05:39 -0300

    refactor: migrate archives container

--- proposal-management-api/docker/init.sh
+++ proposal-management-api/docker/init.sh
@@ -1,28 +1,3 @@
-
-if [ -d "./docker/proposal-management-keycloak-v1" ]; then
-  echo "keycloak ok"
-else
-  cd docker
-  git clone https://github.com/igorcampos-dev/proposal-management-keycloak-v1
-  cd ..
-fi
-
-if [ -d "./docker/proposal-management-rabbitmq-v1" ]; then
-  echo "rabbitmq ok"
-else
-  cd docker
-  git clone https://github.com/igorcampos-dev/proposal-management-rabbitmq-v1
-  cd ..
-fi
-
-if [ -d "./docker/proposal-management-kong-v1" ]; then
-  echo "rabbitmq ok"
-else
-  cd docker
-  git clone https://github.com/igorcampos-dev/proposal-management-kong-v1
-  cd ..
-fi
-
 docker compose up -d --build kong-database-dev
 docker compose up -d --build kong-gateway-dev
 
@@ -35,7 +10,3 @@ docker compose up -d --build keycloak-database-dev
 docker compose up -d --build keycloak-dev
 
 docker compose rm -f deck && docker compose up --force-recreate deck
-
-rm -rf docker/proposal-management-rabbitmq-v1
-rm -rf docker/proposal-management-keycloak-v1
-rm -rf docker/proposal-management-kong-v1
